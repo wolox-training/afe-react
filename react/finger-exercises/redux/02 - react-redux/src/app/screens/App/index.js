394961336ff4581ca8987294cecf214245535066
ArielFerraris <ariel.ferraris@wolox.com.ar>
2019-06-06 15:33:29 -0300

    redux

--- react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
+++ react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
@@ -3,6 +3,8 @@ import store from '@redux/store';
 import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
 
+import actions from '../../../redux/book/actions';
+
 import Book from './components/Book';
 import Search from './components/Search';
 import ShoppingCart from './components/ShoppingCart';
@@ -11,7 +13,8 @@ import styles from './styles.scss';
 class App extends Component {
   state = {
     books: [],
-    bookSelected: []
+    bookSelected: [],
+    length: 0
   };
 
   componentDidMount() {
@@ -19,20 +22,24 @@ class App extends Component {
       const { books, bookSelected } = store.getState();
       this.setState({ books, bookSelected });
     });
-    // TODO to implement the dispatch
+    store.dispatch(actions.getBooks());
   }
 
-  // TODO to implement the dispatch
-  onSearch = value => {};
+  onSearch = value => {
+    store.dispatch(actions.searchBook(value));
+  };
 
-  // TODO to implement the dispatch
-  addToCart = item => {};
+  addToCart = item => {
+    store.dispatch(actions.addToCart(item));
+  };
 
-  // TODO to implement the dispatch
-  addItem = itemId => {};
+  addItem = itemId => {
+    store.dispatch(actions.addItem(itemId));
+  };
 
-  // TODO to implement the dispatch
-  removeItem = itemId => {};
+  removeItem = itemId => {
+    store.dispatch(actions.removeItem(itemId));
+  };
 
   CONFIGURATION_BUTTON = {
     add: {
--- react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
+++ react/finger-exercises/redux/02 - react-redux/src/redux/book/reducer.js
@@ -3,21 +3,65 @@ import { actions } from './actions';
 const initialState = {
   books: [],
   bookSelected: [],
-  originalData: []
+  originalData: [],
+  length: 0
 };
 
 function reducer(state = initialState, action) {
+  if (state.originalData.length === 0) {
+    state.originalData = state.books;
+  }
+  const arr = [];
   switch (action.type) {
     case actions.GET_BOOKS: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        books: [...action.payload]
+      };
     case actions.ADD_TO_CART: // TODO to implement the logic
-      return { ...state };
+      return {
+        ...state,
+        bookSelected: [...state.bookSelected, action.payload]
+      };
     case actions.ADD_ITEM: // TODO to implement the logic
-      return { ...state };
+      state.bookSelected.filter(book => {
+        if (book.id === action.payload) {
+          book.quantity++;
+        }
+      });
+      return {
+        ...state,
+        bookSelected: [...state.bookSelected]
+      };
     case actions.REMOVE_ITEM: // TODO to implement the logic
-      return { ...state };
+      state.bookSelected.filter(book => {
+        if (book.id !== action.payload) {
+          arr.push(book);
+        }
+      });
+      return {
+        ...state,
+        bookSelected: arr
+      };
     case actions.SEARCH_ITEM: // TODO to implement the logic
-      return { ...state };
+      if (action.payload.length <= state.length) {
+        return {
+          ...state,
+          books: state.originalData,
+          length: state.length - 1
+        };
+      }
+      state.books.filter(book => {
+        if (book.name.toLowerCase().includes(action.payload)) {
+          arr.push(book);
+        }
+      });
+      return {
+        ...state,
+        books: arr,
+        length: state.length + 1
+      };
+
     default:
       return state;
   }
